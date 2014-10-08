348d1357c5a4dbc6e744d9d697a1e122c86b234f
lmhtq <lmhtq1991@gmail.com>
2014-10-09 02:03:12 +0800

    awarenwss1

--- aware.c
+++ aware.c
@@ -4,6 +4,8 @@
 #include "aware.h"
 
 /* SAP functions */
+
+//below no use
 /* alloc a node */
 void sap_sta_node_alloc(sap_sta_node *p_ssn)
 {
@@ -35,6 +37,233 @@ void sap_sta_node_delete(char *s)
 		p = p->next;
 	}
 }
+
+//above no use
+
+
+/* get numbers of nodes */
+int get_number_of_nodes(void)
+{
+	int numbers;
+	FILE *fp = popen("iw dev wlp2s0 station dump | head -1 | awk \'{print $1}\'" ,"r");
+	char tmp[10];
+	int len = 32;
+	fgets(tmp, len, fp);
+	pclose(fp);
+	sscanf(tmp, "%d", &numbers);
+	//printf("%s\n", tmp);
+	return numbers;
+}
+
+/* alloc n nodes */
+void sap_sta_node_alloc_n(sap_sta_node *p_ssn, int n_node)
+{
+	p_ssn = (sap_sta_node*)malloc(n_node * sizeof(sap_sta_node));
+}
+
+/* free n nodes */
+void sap_sta_node_free_n(sap_sta_node *p_ssn)
+{
+	free(p_ssn);
+	p_ssn = NULL;
+}
+
+
+/* set n nodes a certain parameter */
+void sap_sta_node_n_set_parameter(int numbers)
+{
+	FILE *fp = popen("iw dev wlp2s0 station dump | sed \'s/://g\' " ,"r");
+	/*
+	Station d0aeec9987e8 (on wlp2s0)
+	inactive time	15196 ms
+	rx bytes	18869519
+	rx packets	180974
+	tx bytes	41526
+	tx packets	1629
+	tx retries	1870
+	tx failed	10
+	signal  	-50 [-61, -51] dBm
+	signal avg	-51 [-65, -51] dBm
+	tx bitrate	1.0 MBit/s
+	rx bitrate	130.0 MBit/s MCS 15
+	authorized	yes
+	authenticated	yes
+	preamble	long
+	WMM/WME	yes
+	MFP		no
+	TDLS peer	no
+	*/
+	char tmp[100];
+	char tmp1[50], tmp2[50];
+	int len = 100;
+	int i;
+	for(i = 0; i < numbers; i++)
+	{
+		fgets(tmp, len, fp);//Station d0aeec9987e8 (on wlp2s0)
+		sscanf(tmp, "%s %x", tmp1, &pHead[i].mac_addr);
+
+		fgets(tmp, len, fp);//inactive time	15196 ms
+		
+		fgets(tmp, len, fp);//rx bytes	18869519
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &pHead[i].rx_bytes);
+		
+		fgets(tmp, len, fp);//rx packets	180974
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &pHead[i].rx_packets);
+		
+		fgets(tmp, len, fp);//tx bytes	41526
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &pHead[i].tx_bytes);
+
+		fgets(tmp, len, fp);//tx packets	1629
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &pHead[i].tx_packets);
+
+		fgets(tmp, len, fp);//tx retries	1870
+		
+		fgets(tmp, len, fp);//tx failed	10
+		
+		fgets(tmp, len, fp);//signal  	-50 [-61, -51] dBm
+		sscanf(tmp, "%s %d", tmp1, &pHead[i].signal);
+		
+		fgets(tmp, len, fp);//signal avg	-51 [-65, -51] dBm
+		
+		fgets(tmp, len, fp);//tx bitrate	1.0 MBit/s
+		sscanf(tmp, "%s %s %f", tmp1, tmp2, &pHead[i].tx_bitrate);
+
+		fgets(tmp, len, fp);//rx bitrate	130.0 MBit/s MCS 15
+		sscanf(tmp, "%s %s %f", tmp1, tmp2, &pHead[i].rx_bitrate);
+
+
+		fgets(tmp, len, fp);//authorized	yes
+		fgets(tmp, len, fp);//authenticated	yes
+		fgets(tmp, len, fp);//preamble	long
+		fgets(tmp, len, fp);//WMM/WME	yes
+		fgets(tmp, len, fp);//MFP		no
+		fgets(tmp, len, fp);//TDLS peer	no
+	}
+	pclose(fp);
+	//printf("%s\n", tmp);
+}
+
+
+/* AC */
+
+/* get numbers of SAP */
+int get_number_of_sap(void)
+{
+	int numbers;
+	FILE *fp = popen("iw dev wlp2s0 station dump | head -1 | awk \'{print $1}\'" ,"r");
+	char tmp[10];
+	int len = 32;
+	fgets(tmp, len, fp);
+	pclose(fp);
+	sscanf(tmp, "%d", &numbers);
+	//printf("%s\n", tmp);
+	return numbers;
+}
+
+/* allocate n SAPs */
+void sap_node_alloc_n(sap_node *p_sn)
+{
+	p_ssn = (sap_sta_node*)malloc(n_node * sizeof(sap_sta_node));
+}
+
+/* free n SAPs */
+void sap_node_free_n(sap_node *p_sn)
+{
+	free(p_ssn);
+	p_ssn = NULL;
+}
+
+/* set n nodes with certain parameters */
+void sap_node_n_set_parameter(int numbers);
+{
+	FILE *fp = popen("iw dev wlp2s0 station dump | sed \'s/://g\' " ,"r");
+	/*
+	Station d0aeec9987e8 (on wlp2s0)
+	inactive time	15196 ms
+	rx bytes	18869519
+	rx packets	180974
+	tx bytes	41526
+	tx packets	1629
+	tx retries	1870
+	tx failed	10
+	signal  	-50 [-61, -51] dBm
+	signal avg	-51 [-65, -51] dBm
+	tx bitrate	1.0 MBit/s
+	rx bitrate	130.0 MBit/s MCS 15
+	authorized	yes
+	authenticated	yes
+	preamble	long
+	WMM/WME	yes
+	MFP		no
+	TDLS peer	no
+	*/
+	char tmp[100];
+	char tmp1[50], tmp2[50];
+	int len = 100;
+	int i;
+	for(i = 0; i < numbers; i++)
+	{
+		fgets(tmp, len, fp);//Station d0aeec9987e8 (on wlp2s0)
+		sscanf(tmp, "%s %x", tmp1, &p_saphead[i].mac_addr);
+
+		fgets(tmp, len, fp);//inactive time	15196 ms
+		
+		fgets(tmp, len, fp);//rx bytes	18869519
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &p_saphead[i].rx_bytes);
+		
+		fgets(tmp, len, fp);//rx packets	180974
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &p_saphead[i].rx_packets);
+		
+		fgets(tmp, len, fp);//tx bytes	41526
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &p_saphead[i].tx_bytes);
+
+		fgets(tmp, len, fp);//tx packets	1629
+		sscanf(tmp, "%s %s %d", tmp1, tmp2, &p_saphead[i].tx_packets);
+
+		fgets(tmp, len, fp);//tx retries	1870
+		
+		fgets(tmp, len, fp);//tx failed	10
+		
+		fgets(tmp, len, fp);//signal  	-50 [-61, -51] dBm
+		sscanf(tmp, "%s %d", tmp1, &p_saphead[i].signal);
+		
+		fgets(tmp, len, fp);//signal avg	-51 [-65, -51] dBm
+		
+		fgets(tmp, len, fp);//tx bitrate	1.0 MBit/s
+		sscanf(tmp, "%s %s %f", tmp1, tmp2, &p_saphead[i].tx_bitrate);
+
+		fgets(tmp, len, fp);//rx bitrate	130.0 MBit/s MCS 15
+		sscanf(tmp, "%s %s %f", tmp1, tmp2, &p_saphead[i].rx_bitrate);
+
+
+		fgets(tmp, len, fp);//authorized	yes
+		fgets(tmp, len, fp);//authenticated	yes
+		fgets(tmp, len, fp);//preamble	long
+		fgets(tmp, len, fp);//WMM/WME	yes
+		fgets(tmp, len, fp);//MFP		no
+		fgets(tmp, len, fp);//TDLS peer	no
+	}
+	pclose(fp);
+	//printf("%s\n", tmp);
+}
+
+
+/* channel table */
+
+/* allocate n channel table */
+void channel_table_alloc_n(int numbers)
+{
+	p_channel_table = (channel_info*)malloc(numbers * sizeof(channel_info));
+}
+
+/* free n channel table */
+void channel_table_free_n(channel_info *p_ct)
+{
+	free(p_ct);
+	p_ct = NULL;
+}
+
+
 
 int main()
 {
--- aware.h
+++ aware.h
@@ -15,6 +15,18 @@ typedef struct station_node
 	/* mac address */
 	char mac_addr[6];
 
+	/* tx,rx */
+	int rx_bytes;
+	int tx_bytes;
+	int rx_packets;
+	int tx_packets;
+	float rx_bitrate;
+	float tx_bitrate;
+
+	/* signal */
+	int signal;
+	int signal_avg;
+
 	/* frame length */
 
 
@@ -36,3 +48,90 @@ void sap_sta_node_insert(sap_sta_node *p_ssn);
 /* delete a node with a certaion mac address*/
 void sap_sta_node_delete(char *s);
 
+/* get numbers of nodes */
+int get_number_of_nodes(void);
+
+/* alloc n nodes */
+void sap_sta_node_alloc_n(sap_sta_node *p_ssn, int n_node);
+
+/* free n nodes */
+void sap_sta_node_free_n(sap_sta_node *p_ssn);
+
+/* set n nodes a certain parameter */
+void sap_sta_node_n_set_parameter(int numbers);
+
+
+
+/* SAP's info of a AC */
+typedef struct sap_node
+{
+	/* supported rates */
+	uchar rate[RATENUM];
+
+	/* mac address */
+	char mac_addr[6];
+
+	/* tx,rx */
+	int rx_bytes;
+	int tx_bytes;
+	int rx_packets;
+	int tx_packets;
+	float rx_bitrate;
+	float tx_bitrate;
+
+	/* signal */
+	int signal;
+	int signal_avg;
+
+	/* frame length */
+
+
+	/* client of a SAP*/
+	struct station_node *pclient;
+} sap_node;
+
+/* head pointer*/
+sap_node *p_saphead;
+
+/* number of SAP */
+int numbers_of_sap;
+
+/* get numbers of SAP */
+int get_number_of_sap(void);
+
+/* allocate n SAPs */
+void sap_node_alloc_n(sap_node *p_sn);
+
+/* free n SAPs */
+void sap_node_free_n(sap_node *p_sn);
+
+/* set n nodes with certain parameters */
+void sap_node_n_set_parameter(int numbers);
+
+
+/* channel table */
+typedef struct chan_info
+{
+	char mac_sap[6]; //sap's mac address
+	int channel;		//sap's current channel
+} channel_info;
+
+channel_info *p_channel_table;
+
+/* allocate n channel table */
+void channel_table_alloc_n(int numbers);
+
+/* free n channel table */
+void channel_table_free_n(channel_info *p_ct);
+
+/* mapping table */
+typedef struct map
+{
+	char mac_master[6];
+	char mac_client[6];	
+} mapping_info;
+
+mapping_info *p_mapping_table;
+
+/* get sum number of clients */
+int get_sum_of_clients()
