e6f367a6d36176c9872d20e1b9f48a0bfe3328dd
lmhtq <lmhtq1991@gmail.com>
2014-10-27 03:51:13 +0800

    script

--- config/dhcpdns.sh
+++ config/dhcpdns.sh
@@ -4,6 +4,7 @@ if [ $# -lt 2 ]; then
 fi
 #get iface
 iface=$(ifconfig -a | grep wlp | sed 's/://g' | awk '{print $1}')
+eiface=$(ifconfig -a | grep enp | sed 's/://g' | awk '{print $1}')
 echo listen-address=$1.1 > $2
 echo interface=$iface >> $2
 echo 'bind-interfaces' >> $2
@@ -12,7 +13,7 @@ echo dhcp-option=6,114.114.114.114,8.8.8.8 >> $2
 
 ifconfig $iface $1.1 netmask 255.255.255.0
 
-iptables -t nat -A POSTROUTING -o $iface -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $eiface -j MASQUERADE
 iptables-save > /etc/iptables/iptables.rules
 
 cp ./$2 /etc/dnsmasq.conf
