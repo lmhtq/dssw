e03ef15e5e7bae0739a463d3e211e261fdf98694
lmhtq <lmhtq1991@gmail.com>
2014-10-27 03:28:42 +0800

    script

--- config/dhcpdns.sh
+++ config/dhcpdns.sh
@@ -7,12 +7,12 @@ iface=$(ifconfig -a | grep wlp | sed 's/://g' | awk '{print $1}')
 echo listen-address=$1.1 > $2
 echo interface=$iface >> $2
 echo 'bind-interfaces' >> $2
-echo dhcp-range $1.100,$1.200,12h >> $2
+echo dhcp-range=$1.100,$1.200,12h >> $2
 echo dhcp-option=6,114.114.114.114,8.8.8.8 >> $2
 
 ifconfig $iface $1.1 netmask 255.255.255.0
 
-iptables -t -nat -A POSTROUTING -o $iface -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $iface -j MASQUERADE
 iptables-save > /etc/iptables/iptables.rules
 
 cp ./$2 /etc/dnsmasq.conf
