06c98029bded32ae883342343781cfb15b5562e2
lmhtq <lmhtq1991@gmail.com>
2014-10-27 03:23:27 +0800

    script

--- config/dhcpdns.sh
+++ config/dhcpdns.sh
@@ -5,10 +5,10 @@ fi
 #get iface
 iface=$(ifconfig -a | grep wlp | sed 's/://g' | awk '{print $1}')
 echo listen-address=$1.1 > $2
-echo interface=$iface >> S2
-echo 'bind-interfaces' >> S2
-echo dhcp-range $1.100,$1.200,12h >> S2
-echo dhcp-option=6,114.114.114.114,8.8.8.8 >> S2
+echo interface=$iface >> $2
+echo 'bind-interfaces' >> $2
+echo dhcp-range $1.100,$1.200,12h >> $2
+echo dhcp-option=6,114.114.114.114,8.8.8.8 >> $2
 
 ifconfig $iface $1.1 netmask 255.255.255.0
 
